bc98b092958105aa2ab565e06f5682ff737b840e
Manav Shergill <shergillmanav@uvic.ca>
2022-02-05 14:46:33 -0800

    aws s3 flow working

--- index.js
+++ index.js
@@ -1,78 +1,100 @@
 const express = require("express");
 const fs = require("fs");
 const util = require("util");
+const aws = require("aws-sdk");
 const sharp = require("sharp");
 const axios = require("axios");
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
+const { videoResize } = require("node-video-resize");
 ffmpeg.setFfmpegPath(ffmpegPath);
 // const upload = multer({ dest: "uploads/" });
-const { uploadFile, getFileStream } = require("./s3");
+const { uploadFile, getFileStream, fileExists } = require("./s3");
 const app = express();
 
-app.post("/uploadtos3", async (req, res) => {
-  console.log(req.query);
-  const key = req.query.key;
-  const type = req.query.type;
+const S3 = require("aws-sdk/clients/s3");
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
 
-  
-
-    try {
-      const res = await axios({
-        url: `https://ipfs.io/ipfs/${key}`,
-        responseType: "arraybuffer",
-      });
-      const buffer = Buffer.from(res.data, "binary");
-      
-      if(type=="image"){
-        const image = await sharp(buffer)
-          .resize(300, null)
-          .withMetadata()
-          .toBuffer({ resolveWithObject: true });
-          await uploadFile(key,image.data);
-      }
-      if(type=="video"){
-         fs.writeFile(`${key}.mp4`, buffer,{encoding: 'base64'}, (err) => {
-          if(!err) console.log('Data written');
-          // ffmpeg(`./${key}.mp4`).size('200x?');
-          ffmpeg(`${key}.mp4`)
-          .output('outputfile.mp4')
-          .videoCodec('libx264')
-          .size("30%")
-          .on('end', function() {
-            console.log('Finished processing');
-          })
-          .run();
-          
-
-        });
-        
-        fs.readFile('outputfile.mp4', {encoding: 'base64'} , async  (err, data) => {
-          if (err) {
-            console.error(err)
-            return
-          }
-          await uploadFile(key,data);
-        })
-        
-      }
-    } catch (error) {
-      console.error(`error occurred ${error}`);
-      res.send("Error");
-    }
-  
-
-  res.send("Success");0 
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
 });
+async function uploadtos3(key, type) {
+  try {
+    const res = await axios({
+      url: `https://ipfs.io/ipfs/${key}`,
+      responseType: "arraybuffer",
+    });
+    const buffer = Buffer.from(res.data, "binary");
 
-app.get("/getImage", (req, res) => {
-  // console.log(req.params);
-  // const key = req.params.key;
-  const readStream = getFileStream(
-    "QmPR5rU9UbGVhFiMbUrSEZWy6rAAvSZwMscBekMcwXy5AQ"
+    if (type == "image") {
+      const image = await sharp(buffer)
+        .resize(250, null)
+        .withMetadata()
+        .toBuffer({ resolveWithObject: true });
+      await uploadFile(key, image.data, type);
+    }
+    if (type == "video") {
+      fs.writeFile(
+        `${key}.mp4`,
+        buffer,
+        // { encoding: "base64" },
+        async (err) => {
+          if (!err) console.log("Data written");
+          // ffmpeg(`./${key}.mp4`).size('200x?');
+          // ffmpeg(`${key}.mp4`)
+          //   .output("outputfile.mp4")
+          //   .videoCodec("h.264")
+          //   .size("30%")
+          //   .on("end", function () {
+          //     console.log("Finished processing");
+          //   })
+          //   .run();
+          await videoResize({
+            inputPath: `${key}.mp4`,
+            outputPath: "output.mp4",
+            format: "mp4",
+            size: "250x350",
+          });
+          fs.readFile("output.mp4", async (err, data) => {
+            if (!err) await uploadFile(key, data, type);
+          });
+        }
+      );
+    }
+  } catch (error) {
+    console.error(`error occurred ${error}`);
+    return "Error";
+  }
+
+  return "Success";
+}
+
+app.get("/file/:key/:type", async (req, res) => {
+  console.log(req.params);
+  const key = req.params.key;
+  const type = req.params.type;
+
+  s3.headObject(
+    {
+      Bucket: bucketName,
+      Key: key,
+    },
+    function (err, data) {
+      if (err) {
+        // file does not exist
+        uploadtos3(key, type);
+        return { Error: "Error" };
+      } else {
+        const readStream = getFileStream(key);
+        readStream.pipe(res);
+      }
+    }
   );
-
-  readStream.pipe(res);
 });
 
 app.listen(8080, () => console.log("listening on port 8080"));
--- s3.js
+++ s3.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
-const fs = require("fs");
 const S3 = require("aws-sdk/clients/s3");
-const sharp = require("sharp");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
@@ -13,36 +11,26 @@ const s3 = new S3({
   accessKeyId,
   secretAccessKey,
 });
-async function ShrinkSize(path, imageFit = "fill", width = 235, height = 320) {
-  const resizeOptions = {
-    fit: imageFit,
-  };
-  const image = await sharp(path)
-    .resize(width, height, resizeOptions)
-    .withMetadata()
-    .toBuffer({ resolveWithObject: true });
-  return image;
-}
-// uploads a file to s3
-function uploadFile(key, res) {
 
+// uploads a file to s3
+function uploadFile(key, res, type) {
+  console.log(res);
   const uploadParams = {
     Bucket: bucketName,
     Body: res,
     Key: key,
-    ContentType: "video/mp4"
+    ContentType: type === "video" ? "video/mp4" : "image/png",
   };
   return s3.upload(uploadParams).promise();
 }
 exports.uploadFile = uploadFile;
 
-// downloads a file from s3
 function getFileStream(fileKey) {
-  const downloadParams = {
+  const params = {
     Key: fileKey,
     Bucket: bucketName,
   };
 
-  return s3.getObject(downloadParams).createReadStream();
+  return s3.getObject(params).createReadStream();
 }
 exports.getFileStream = getFileStream;
